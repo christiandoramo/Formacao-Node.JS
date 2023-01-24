87280e8303de4fcbc4d41be0f2397eea04cefdfa
christiandoramo <christiandoramo@gmail.com>
2023-01-24 01:34:46 -0300

    inicio

---
+++ inicio/app.js
@@ -0,0 +1,8 @@
+let c = require("./calculadora")
+console.log("Hello World!")
+
+console.log(c.add(20,10))
+console.log(c.sub(20,10))
+console.log(c.mul(20,10))
+console.log(c.div(20,10))
+console.log(c.nome)
---
+++ inicio/calculadora.js
@@ -0,0 +1,16 @@
+function add(a, b) {
+  return a + b
+}
+function sub(a, b) {
+  return a - b
+}
+function mul(a, b) {
+  return a * b
+}
+function div(a, b) {
+  return a / b
+}
+
+let nome = 'Eu sou o Christian'
+
+module.exports = { add, sub, mul, div, nome }
